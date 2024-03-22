8595e1836144d04fdd197a0ed4baccfa8bf0ba74
antonio2468 <cortesantonio822@gmail.com>
2024-03-22 10:58:11 -0600

    version 2

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'Tienda-Ionic',
+  appName: 'product-admin-ionic',
   webDir: 'www',
   server: {
     androidScheme: 'https'
